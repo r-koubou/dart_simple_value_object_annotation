a1802b09df2d192909823f5c2df638057c01fc2d
Hiroaki@R-Koubou <dev.r.koubou@gmail.com>
2025-12-07 17:20:16 +0900

    allowEmpty指定を追加

--- lib/value_object_annotation.dart
+++ lib/value_object_annotation.dart
@@ -1,31 +1,37 @@
-/// An annotation to define a value object with optional constraints.
+/// An annotation to define a value object with optional constraints for code generation.
 ///
 /// [T]: The data type of the value object (e.g., String, int).
-/// [min]: The minimum value for numeric types.
-/// [max]: The maximum value for numeric types.
-/// [minLength]: The minimum length for string types.
-/// [maxLength]: The maximum length for string types.
-/// [comparable]: Whether the value object should implement comparison operators.
 ///
 /// Example usage:
 /// ```dart
 /// import 'package:simple_value_object_annotation/value_object_annotation.dart';
 ///
-/// @ValueObject(String, minLength: 3, maxLength: 50)
-/// class Username {}
+/// @ValueObject<String>(minLength: 5, maxLength: 80)
+/// typedef Username = _$Username;
 /// ```
 class ValueObject<T> {
   /// The minimum value for numeric types.
+  /// if null, no minimum constraint is applied.
   final num? min;
 
   /// The maximum value for numeric types.
+  /// if null, no maximum constraint is applied.
   final num? max;
 
-  /// The minimum length for string types.
+  /// The minimum length for value types.
+  /// A code generator assumes that data types possess a `length` property when generating code.
+  /// if null, no minimum length constraint is applied.
   final int? minLength;
 
-  /// The maximum length for string types.
+  /// The maximum length for value types.
+  /// A code generator assumes that data types possess a `length` property when generating code.
+  /// if null, no maximum length constraint is applied.
   final int? maxLength;
+
+  /// Whether to allow empty values, like empty strings or collections.
+  /// A code generator assumes that data types possess a `isEmpty` property when generating code.
+  /// if null, validation for emptiness is not applied.
+  final bool? allowEmpty;
 
   /// A map of predefined constant values for the value object.
   final Map<String, T>? presets;
@@ -36,6 +42,7 @@ class ValueObject<T> {
     this.max,
     this.minLength,
     this.maxLength,
+    this.allowEmpty,
     this.presets,
   });
 }
