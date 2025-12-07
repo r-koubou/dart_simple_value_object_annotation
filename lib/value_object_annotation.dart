5b37a33692a9ff4eee5bb06f3cf77f7247ba0c83
Hiroaki@R-Koubou <dev.r.koubou@gmail.com>
2025-12-07 14:57:05 +0900

    プリセット定義を可能に

--- lib/value_object_annotation.dart
+++ lib/value_object_annotation.dart
@@ -1,36 +1,41 @@
 /// An annotation to define a value object with optional constraints.
-/// 
-/// [type]: The data type of the value object (e.g., String, int).
+///
+/// [T]: The data type of the value object (e.g., String, int).
 /// [min]: The minimum value for numeric types.
 /// [max]: The maximum value for numeric types.
 /// [minLength]: The minimum length for string types.
 /// [maxLength]: The maximum length for string types.
-/// 
+/// [comparable]: Whether the value object should implement comparison operators.
+///
 /// Example usage:
 /// ```dart
 /// import 'package:simple_value_object_annotation/value_object_annotation.dart';
-/// 
+///
 /// @ValueObject(String, minLength: 3, maxLength: 50)
 /// class Username {}
 /// ```
-class ValueObject {
-  /// The data type of the value object (e.g., String, int).
-  final Type type;
+class ValueObject<T> {
   /// The minimum value for numeric types.
   final num? min;
+
   /// The maximum value for numeric types.
   final num? max;
+
   /// The minimum length for string types.
   final int? minLength;
+
   /// The maximum length for string types.
   final int? maxLength;
 
+  /// A map of predefined constant values for the value object.
+  final Map<String, T>? presets;
+
   /// Ctor for ValueObject annotation.
-  const ValueObject(
-    this.type, {
+  const ValueObject({
     this.min,
     this.max,
     this.minLength,
     this.maxLength,
+    this.presets,
   });
 }
